5c8020db953a067569c612d02cafb8226941962a
diogomsp97 <diogomsp97@gmail.com>
2020-09-02 12:31:42 +0100

    Create, Update, and Delete Posts

--- djangoProject/blog/urls.py
+++ djangoProject/blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blogHome'),
+    path('', PostListView.as_view(), name='blogHome'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='postDetail'),
+    path('post/new/', PostCreateView.as_view(), name='postCreate'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='postUpdate'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='postDelete'),
     path('about/', views.about, name='blogAbout'),
 ]
